fb7ed17fc42896fbb97de9b39b73a69d854472f7
Cesar <cesarbrazon10@gmail.com>
2024-06-18 01:34:36 +0200

    chore: send tokens with autotx uses local llm

--- submodules/moragents_dockers/agents/src/send_agent/src/agent.py
+++ submodules/moragents_dockers/agents/src/send_agent/src/agent.py
@@ -1,11 +1,14 @@
+import json
 import os
 import logging
+import traceback
+
 from .config import Config
 
 os.environ["ALCHEMY_API_KEY"] = Config.ALCHEMY_API_KEY
 
-from autotx import AutoTx
-from autotx.AutoTx import Config as AutoTxConfig
+from autotx import AutoTx, LlamaClient
+from autotx.AutoTx import Config as AutoTxConfig, CustomModel
 from autotx.utils.ethereum.networks import NetworkInfo
 from autotx.wallets.smart_wallet import SmartWallet
 from autotx.agents.SendTokensAgent import SendTokensAgent
@@ -13,10 +16,11 @@ from autotx.utils.ethereum.eth_address import ETHAddress
 from web3 import Web3, HTTPProvider
 from flask import jsonify
 
-available_agents = [SendTokensAgent()]
-autotx_config = AutoTxConfig(Config.VERBOSE, Config.LLM_CONFIG, None)
-
 messages = []
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def chat(request, llm):
@@ -25,19 +29,32 @@ def chat(request, llm):
         if "prompt" in data:
             prompt = data["prompt"]
             network = NetworkInfo.from_chain_id(data["chain_id"])
-            rpc_url = network.get_subsidized_rpc_url()
-            w3 = Web3(HTTPProvider(rpc_url))
+            w3 = Web3(HTTPProvider(network.get_subsidized_rpc_url()))
             wallet = SmartWallet(w3, ETHAddress(data["wallet_address"]))
             messages.append({"role": "user", "content": prompt})
-            autotx = AutoTx(w3, wallet, network, available_agents, autotx_config)
-            result = autotx.run(prompt, True, "reflection_with_llm")
-            logging.info(result.transactions)
-            messages.append({"role": "assistant", "content": result.summary})
-            return jsonify({"role": "assistant", "content": result.summary})
+            custom_model = CustomModel(client=LlamaClient, arguments={"llm": llm})
+            autotx_config = AutoTxConfig(
+                Config.VERBOSE, Config.LLM_CONFIG, None, custom_model=custom_model
+            )
+            autotx = AutoTx(w3, wallet, network, [SendTokensAgent()], autotx_config)
+            result = autotx.run(prompt, True, summary_method="reflection_with_llm")
+            logging.info(result.intents)
+
+            try:
+                summary = json.loads(result.summary)
+                if "content" in summary:
+                    message = {"role": "assistant", "content": summary["content"]}
+                    messages.append(message)
+                    return jsonify(message)
+            except:
+                messages.append({"role": "assistant", "content": result.summary})
+                return jsonify(result.summary)
         else:
             return jsonify({"error": "Missing required parameters"}), 400
 
     except Exception as e:
+        logging.error("Exception occurred: %s", e)
+        traceback.print_exc()
         return jsonify({"Error": str(e)}), 500
 
 
--- submodules/moragents_dockers/agents/src/send_agent/src/config.py
+++ submodules/moragents_dockers/agents/src/send_agent/src/config.py
@@ -3,12 +3,6 @@ class Config:
     ALCHEMY_API_KEY = "NRQxqrVyo02u4NK55FNKjJep_xEm6n7b"
     VERBOSE = True
     LLM_CONFIG = lambda: {
-        "cache_seed": None,
-        "config_list": [
-            {
-                "model": "gpt-4o",
-                "api_key": "",
-                "base_url": "https://api.openai.com/v1",
-            }
-        ],
+        "model_client_cls": "LlamaClient",
+        "model": "meetkai/functionary-small-v2.4-GGUF"
     }
