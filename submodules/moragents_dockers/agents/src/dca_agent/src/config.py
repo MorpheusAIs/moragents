9ebe26c5a134fb78ef85a9a3af82acff2c4d1081
domsteil <domsteil14@gmail.com>
2024-09-24 11:43:19 -0700

    updating api

---
+++ submodules/moragents_dockers/agents/src/dca_agent/src/config.py
@@ -0,0 +1,11 @@
+DELEGATOR_CONFIG = {
+    "agents": [
+        {
+            "path": "dca_agent.agent",
+            "class": "DCAAgent",
+            "description": "If the user wants to set up a dollar-cost averaging strategy for crypto purchases.",
+            "name": "DCA Agent",
+            "upload": False
+        }
+    ]
+}
